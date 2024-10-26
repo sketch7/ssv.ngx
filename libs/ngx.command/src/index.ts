25c18be2d38786da06a3674f2987bdbd10f247fd
Stephen Lautier <stephen.lautier@outlook.com>
2024-10-26 20:05:02 +0200

    move command files to library

--- libs/ngx.command/src/index.ts
+++ libs/ngx.command/src/index.ts
@@ -1,1 +1,8 @@
-export * from './lib/ngx.command/ngx.command.component';
+export * from "./command";
+export * from "./command.directive";
+export * from "./command-ref.directive";
+export * from "./command.util";
+export { CommandCreator, ICommand } from "./command.model";
+export * from "./config";
+export * from "./module";
+export * from "./version";
