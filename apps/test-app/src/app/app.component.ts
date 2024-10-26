f83807cb65e9a86e98f02ae0fe323bd542c8944e
Stephen Lautier <stephen.lautier@outlook.com>
2024-10-26 19:52:58 +0200

    Feature/nx init (#1)
    * regen + add lib
    
    * add test app
    
    * use command in test-app

---
+++ apps/test-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgxCommandComponent } from "@ssv/ngx.command";
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, NgxCommandComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'test-app';
+}
