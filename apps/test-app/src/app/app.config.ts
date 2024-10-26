4257e9fbfc42f05a783e9968bda8e96afd845336
Stephen Lautier <stephen.lautier@outlook.com>
2024-10-27 01:56:21 +0200

    initial example app + change to partial opts

--- apps/test-app/src/app/app.config.ts
+++ apps/test-app/src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideSsvCommandOptions } from "@ssv/ngx.command";
+
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-  ],
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(appRoutes),
+
+		provideSsvCommandOptions({
+			executingCssClass: "is-busy",
+			hasDisabledDelay: false
+		})
+	],
 };
