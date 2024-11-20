1a0b8b288ee91f9a9a2b7380f31e3ef7c553636f
Stephen Lautier <stephen.lautier@outlook.com>
2024-11-20 22:39:48 +0100

    gen lib

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,24 @@
-const nx = require('@nx/eslint-plugin');
+const nx = require("@nx/eslint-plugin");
 
 module.exports = [
-	...nx.configs['flat/base'],
-	...nx.configs['flat/typescript'],
-	...nx.configs['flat/javascript'],
+	...nx.configs["flat/base"],
+	...nx.configs["flat/typescript"],
+	...nx.configs["flat/javascript"],
 	{
-		ignores: ['**/dist'],
+		ignores: ["**/dist"],
 	},
 	{
-		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
 		rules: {
-			'@nx/enforce-module-boundaries': [
-				'error',
+			"@nx/enforce-module-boundaries": [
+				"error",
 				{
 					enforceBuildableLibDependency: true,
-					allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+					allow: ["^.*/eslint(\\.base)?\\.config\\.[cm]?js$"],
 					depConstraints: [
 						{
-							sourceTag: '*',
-							onlyDependOnLibsWithTags: ['*'],
+							sourceTag: "*",
+							onlyDependOnLibsWithTags: ["*"],
 						},
 					],
 				},
@@ -26,17 +26,15 @@ module.exports = [
 		},
 	},
 	{
-		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
 		// Override or add rules here
 		rules: {
-			'@angular-eslint/directive-selector': 'off',
+			"@angular-eslint/directive-selector": "off",
 		},
 	},
 	{
-		files: ['**/*.json'],
-		rules: {
-			'@nx/dependency-checks': ['error', { ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'] }],
-		},
-		languageOptions: { parser: require('jsonc-eslint-parser') },
+		files: ["**/*.json"],
+		rules: { "@nx/dependency-checks": ["error", { ignoredFiles: ["{projectRoot}/eslint.config.{js,cjs,mjs}"] }] },
+		languageOptions: { parser: require("jsonc-eslint-parser") },
 	},
 ];
---
+++ libs/ngx.ux/README.md
@@ -0,0 +1,7 @@
+# ngx.ux
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ngx.ux` to execute the unit tests.
